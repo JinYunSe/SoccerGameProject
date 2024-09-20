b352d7193db00baef8684d7b90efa1017e9be638
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2024-09-20 13:43:49 +0900

    동적 테이블 조회 및 불필요한 중복 제거

--- src/routers/enforce_router.js
+++ src/routers/enforce_router.js
@@ -1,14 +1,7 @@
 import express from 'express';
 import joi from 'joi';
-import { enforcePlayer, rarityRowCreate } from '../utils/math/enforce.js';
-
-import prisma from '../utils/prisma/index.js';
-
-import {
-  existingHoldPlayer,
-  incrementHoldPlayer,
-  addHoldPlayer,
-} from '../utils/players/holdplayers.js';
+import { enforcePlayer } from '../utils/math/enforce.js';
+import { table_findFirst, row_create } from '../utils/tableFunction/table.js';
 
 const name_validation = joi.object({
   name: joi
@@ -31,24 +24,19 @@ const stats_validation = joi.object({
 const enforce_router = express.Router();
 
 enforce_router.patch('/enforce', async (req, res, next) => {
-  const { name } = await name_validation.validateAsync(req.body);
-  const exist_hold_player = await existingHoldPlayer(1, name);
-  console.log(
-    '선수 이름' +
-      exist_hold_player.name +
-      ', 강화 등급 : ' +
-      exist_hold_player.enforce +
-      ', 개수 : ' +
-      exist_hold_player.count,
-  );
-  console.log(exist_hold_player);
+  const validation = await name_validation.validateAsync(req.body);
+  const exist_hold_player = await table_findFirst(process.env.HOLD_PLAYERS, {
+    account_id: 2,
+    ...validation,
+  });
+  // 향후 인증 인가를 바탕으로 한 req.account_id로 바꾸기
+
   if (!exist_hold_player) return res.status(404).json('강화할 선수가 없습니다.');
 
   if (exist_hold_player.enforce >= exist_hold_player.count)
     return res.status(402).json('강화 재화가 부족합니다.');
 
   const result = await enforcePlayer(exist_hold_player.id, exist_hold_player.enforce);
-  // 향후 인증 인가를 바탕으로 req.account_id로 바꾸기
   return res.status(201).json(result);
 });
 
@@ -57,8 +45,7 @@ enforce_router.post('/rarity', async (req, res, next) => {
     const validation = await stats_validation.validateAsync(req.body);
     const rarity = validation.rarity;
     delete validation.rarity;
-
-    await rarityRowCreate(rarity, { ...validation });
+    await row_create(rarity, { ...validation });
     return res.status(200).json(`${rarity} 등급 STATS 증가가 등록 됐습니다`);
   } catch (error) {
     next(error);
---
+++ src/utils/tableFunction/table.js
@@ -0,0 +1,42 @@
+import prisma from '../prisma/index.js';
+
+const table_findFirst = async (table_name, value) => {
+  return await prisma[table_name].findFirst({
+    where: { ...value },
+  });
+};
+
+const table_findMany = async (table_name, value) => {
+  return await prisma[table_name].findMany({
+    where: { ...value },
+  });
+};
+
+const row_update = async (table_name, where_condition, value) => {
+  await prisma[table_name].update({
+    where: { ...where_condition },
+    data: {
+      ...value,
+    },
+  });
+};
+
+const row_create = async (table_name, value) => {
+  await prisma[table_name].create({
+    data: {
+      ...value,
+    },
+  });
+};
+
+const row_delete = async (table_name, where_condition) => {
+  await prisma[table_name].delete({
+    where: { ...where_condition },
+  });
+};
+
+// 위의 함수들을 좀 더 잘 활용하면
+// 모든 input(배열, 객체, 변수)에 대해
+// create, delete, select, update가 동작될 수 있게
+// 할 수 있을 것 같은데... 아직은 잘 모르겠습니다...
+export { table_findFirst, table_findMany, row_update, row_create, row_delete };
