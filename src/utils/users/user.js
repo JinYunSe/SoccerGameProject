ff7d7c7ce35970f9652c8aa9b2aeb74f568c8d95
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2024-09-25 22:49:06 +0900

    함수화를 안 했지만... 마음에 안 드는 주석과 변수 이름 변경

--- src/utils/users/user.js
+++ src/utils/users/user.js
@@ -1,14 +1,16 @@
 import { table_findMany } from '../tableFunction/table.js';
 
 // 상대방 유저들 찾기
-const findOpponentUsers = async (my_account_id, currentPoint, count) => {
+const findOpponentUsers = async (my_account_id, current_point, count) => {
   let rank_scope = 100 * count;
   return await table_findMany(
     process.env.ACCOUNTS,
     {
       // 본인을 제외한 범위 내 유저들 찾기
       NOT: { account_id: my_account_id },
-      point: { gte: currentPoint - rank_scope, lte: currentPoint + rank_scope },
+      point: { gte: current_point - rank_scope, lte: current_point + rank_scope },
+      // 현재 코드를 본 결과 유저의 밀집도가 낮을 경우 음의 영역까지
+      // 범위를 탐색하는 문제가 발생할 수 있다는 사실을 알았다.
     },
     { account_id: true, nickname: true, win: true, draw: true, lose: true, point: true },
   );
